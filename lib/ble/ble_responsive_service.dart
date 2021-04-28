19d1f3be6e51a301034fda87ce1b1b04aaf9e5c9
Taras Rashnik <taras.rashnik@gmail.com>
2021-04-28 19:51:52 +0200

    add fimber

--- lib/ble/ble_responsive_service.dart
+++ lib/ble/ble_responsive_service.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:fimber/fimber.dart';
+
 class BleResponsiveService {
   final _streamController = StreamController<List<int>>();
   bool _stopping = false;
@@ -7,16 +9,22 @@ class BleResponsiveService {
   Stream<List<int>> get samplesStream => _streamController.stream;
 
   Future<void> start() async {
+    Fimber.i("BleResponsiveService.start().");
+
     int counter = 10;
     while (!_stopping) {
+      // Fimber.d("Send samples from BLE service.");
       _streamController.sink.add(List<int>.filled(512, counter++));
       if (counter > 250) counter = 5;
 
       await Future.delayed(Duration(milliseconds: 10));
     }
+
+    Fimber.i("BleResponsiveService.start() exited.");
   }
 
   Future<void> stop() async {
+    Fimber.i("BleResponsiveService.stop().");
     _stopping = true;
     await _streamController.close();
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,20 @@ import 'package:ble_oscilloscope/ble/ble_responsive_service.dart';
 import 'package:ble_oscilloscope/bloc/samples_cubit.dart';
 import 'package:ble_oscilloscope/control_panel_widget.dart';
 import 'package:ble_oscilloscope/scope_widget.dart';
+import 'package:fimber/fimber.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 import 'layout/responsive_layout.dart';
 
 void main() {
-  runApp(RepositoryProvider(
+  Fimber.plantTree(DebugTree());
+  Fimber.i("Start app.");
+
+  runApp(Provider<BleResponsiveService>(
     create: (_) => BleResponsiveService()..start(),
+    dispose: (_, service) => service.stop(),
     child: MyApp(),
   ));
 }
